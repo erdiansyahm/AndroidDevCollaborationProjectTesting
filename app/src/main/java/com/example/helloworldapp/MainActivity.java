2ac7d6c3753db3e95fe9bd6acc4fe40c1c193f08
Bagus Erwanto <57589007+volumeee@users.noreply.github.com>
2024-02-22 21:26:11 +0700

    first commit

---
+++ app/src/main/java/com/example/helloworldapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.helloworldapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private Button button;
+    private int count = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count ++;
+                updateTextView();
+            }
+        });
+    }
+    private void updateTextView(){
+        textView.setText("Hello World\nCount:" + count);
+    }
+}
